92dc730be2c17eb86deead6afa1c625ce57ead5b
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-04-16 18:56:06 -0500

    Updated  Button, Started JavaScript and CSS

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+console.log("javascript sourced!")
+
+function addEmployee(event) {
+    event.preventDefault();
+console.log("Function Started")
+
+// grabbing user inpus 
+let firstName = document.getElementById('firstName').value
+let lastName = document.getElementById('lastName').value
+let id = document.getElementById('id').value
+let title= document.getElementById('title').value 
+let annualSalary = document.getElementById('annualSalary').value
+
+// need new user inputs to print on the table
+ 
+}
