8291a4799e264d86b090f0c008350e6a253e3643
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-04-16 19:07:07 -0500

    Working on table in JS

--- scripts.js
+++ scripts.js
@@ -13,5 +13,15 @@ let title= document.getElementById('title').value
 let annualSalary = document.getElementById('annualSalary').value
 
 // need new user inputs to print on the table
- 
+// gathering strings that will be put into the tables
+let newRow= `
+<tr>
+<td>${firstName}</td>
+<td>${lastName}</td>
+<td>${id}</td>
+<td>${title}</td>
+<td>${annualSalary}</td>
+</tr>
+`
+// console.log("newRow is: ", newRow)
 }
